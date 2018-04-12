adf9bd0fa3895202e59a967300c989adbcc3fd59
t.shulepina <Tamaro4kaaaaa@mail.ru>
2018-04-12 15:00:58 +0300

    Домашняя работа 3

--- address_new.py
+++ address_new.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
-
+from groupadd import Groupadd
 def is_alert_present(wd):
     try:
         wd.switch_to_alert().text
@@ -19,13 +19,13 @@ class address_new(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_add_new_page(wd)
-        self.create_group(wd,
+        self.create_group(wd, Groupadd(
                           name="Anna", middlename="Ivanovna", lastname="Petrova", nickname="AnPet",
                           title="ZAO", company="Romashka", adrdress="Moscow",
                           home="84951234567", mobile="89161234567", work="88001234567", fax="88000000000",
                           email="an@an.ru", email2="anp@an.ru", email3="aniv@an.ru", homapage="annaivanova.ru",
                           byear="1990", ayear="2010",
-                          address2="Lenina street", phone2="83", notes="16")
+                          address2="Lenina street", phone2="83", notes="16"))
         self.logout(wd)
 
     def test_empty_address_new(self):
@@ -33,75 +33,74 @@ class address_new(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_add_new_page(wd)
-        self.create_group(wd,
+        self.create_group(wd, Groupadd(
                           name="", middlename="", lastname="", nickname="",
                           title="", company="", adrdress="",
                           home="", mobile="", work="", fax="",
                           email="", email2="", email3="", homapage="",
                           byear="", ayear="",
-                          address2="", phone2="", notes="")
+                          address2="", phone2="", notes=""))
         self.logout(wd)
 
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
-    def create_group(self, wd, name, middlename, lastname, nickname, title, company, adrdress, home, mobile, work, fax,
-                     email, email2, email3, homapage, byear, ayear, address2, phone2, notes):
+    def create_group(self, wd, groupadd):
         # init add new creation
         wd.find_element_by_name("firstname").click()
         # fill add new form
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(name)
+        wd.find_element_by_name("firstname").send_keys(groupadd.name)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(middlename)
+        wd.find_element_by_name("middlename").send_keys(groupadd.middlename)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(lastname)
+        wd.find_element_by_name("lastname").send_keys(groupadd.lastname)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(nickname)
+        wd.find_element_by_name("nickname").send_keys(groupadd.nickname)
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(title)
+        wd.find_element_by_name("title").send_keys(groupadd.title)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(company)
+        wd.find_element_by_name("company").send_keys(groupadd.company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(adrdress)
+        wd.find_element_by_name("address").send_keys(groupadd.adrdress)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(home)
+        wd.find_element_by_name("home").send_keys(groupadd.home)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(mobile)
+        wd.find_element_by_name("mobile").send_keys(groupadd.mobile)
         wd.find_element_by_name("work").click()
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(work)
+        wd.find_element_by_name("work").send_keys(groupadd.work)
         wd.find_element_by_name("fax").click()
         wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(fax)
+        wd.find_element_by_name("fax").send_keys(groupadd.fax)
         wd.find_element_by_name("theform").click()
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(email)
+        wd.find_element_by_name("email").send_keys(groupadd.email)
         wd.find_element_by_name("email2").click()
         wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(email2)
+        wd.find_element_by_name("email2").send_keys(groupadd.email2)
         wd.find_element_by_name("email3").click()
         wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(email3)
+        wd.find_element_by_name("email3").send_keys(groupadd.email3)
         wd.find_element_by_name("homepage").click()
         wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(homapage)
+        wd.find_element_by_name("homepage").send_keys(groupadd.homapage)
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[3]").is_selected():
             wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[3]").click()
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[2]").is_selected():
             wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[2]").click()
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(byear)
+        wd.find_element_by_name("byear").send_keys(groupadd.byear)
         wd.find_element_by_name("theform").click()
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[4]").is_selected():
             wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[4]").click()
@@ -109,16 +108,16 @@ class address_new(unittest.TestCase):
             wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[3]").click()
         wd.find_element_by_name("ayear").click()
         wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(ayear)
+        wd.find_element_by_name("ayear").send_keys(groupadd.ayear)
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(address2)
+        wd.find_element_by_name("address2").send_keys(groupadd.address2)
         wd.find_element_by_name("phone2").click()
         wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(phone2)
+        wd.find_element_by_name("phone2").send_keys(groupadd.phone2)
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(notes)
+        wd.find_element_by_name("notes").send_keys(groupadd.notes)
         # enter
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
