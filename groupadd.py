ceba5a14df528dce3db1c6804b490bc14b2a899d
t.shulepina <Tamaro4kaaaaa@mail.ru>
2018-04-12 15:01:57 +0300

    Домашняя работа 3

---
+++ groupadd.py
@@ -0,0 +1,28 @@
+
+class Groupadd:
+
+    def __init__(self, name, middlename, lastname, nickname, title, company, adrdress, home, mobile, work, fax, email,
+                  email2, email3, homapage, byear, ayear, address2, phone2, notes):
+        self.name = name
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.adrdress = adrdress
+        self.home = home
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homapage = homapage
+        self.byear = byear
+        self.ayear = ayear
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
+
+
+
