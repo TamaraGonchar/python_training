85288d9dd8b9b8f51f6432d3d4b8e2c24b315358
t.shulepina <Tamaro4kaaaaa@mail.ru>
2018-04-23 18:48:32 +0300

    ДЗ 7

--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -1,9 +1,14 @@
 from model.group import Group
 
-def test_mod_first_group(app):
+def test_mod_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.mod_first_group()
-    app.group.create(Group(name="LOLO", header="KOKO", footer="AYAYAY"))
+    app.group.mod_first_group(Group(name="NEW NAME"))
+    app.session.logout()
+
+
+def test_mod_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.mod_first_group(Group(header="NEW HEADER"))
     app.session.logout()
 
     #DZ 7
