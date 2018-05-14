bb09abf53b1c478bccef270be6b153377991141b
t.shulepina <Tamaro4kaaaaa@mail.ru>
2018-05-14 17:44:24 +0300

    ДЗ 13

---
+++ test/test_phones.py
@@ -0,0 +1,23 @@
+import re
+
+def test_phones_on_home_page(app):
+    addnew_from_home_page = app.addnew.get_addnew_list()[0]
+    addnew_from_edit_page = app.addnew.get_addnew_info_from_edit_page(0)
+    assert addnew_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(addnew_from_edit_page)
+
+def test_phones_on_contact_view_page(app):
+    addnew_from_view_page = app.addnew.get_addnew_from_view_page(0)
+    addnew_from_edit_page = app.addnew.get_addnew_info_from_edit_page(0)
+    assert addnew_from_view_page.home == addnew_from_edit_page.home
+    assert addnew_from_view_page.mobile == addnew_from_edit_page.mobile
+    assert addnew_from_view_page.work == addnew_from_edit_page.work
+    assert addnew_from_view_page.phone2 == addnew_from_edit_page.phone2
+
+def clear(s):
+    return re.sub("[() -]", "", s)
+
+def merge_phones_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear(x),
+                                filter(lambda x: x is not None,
+                                       [contact.home, contact.mobile, contact.work, contact.phone2]))))
