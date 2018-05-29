6f3449071655681cbec9524e88dad78f649662b9
t.shulepina <Tamaro4kaaaaa@mail.ru>
2018-05-29 19:12:38 +0300

    ДЗ 21 и орм

--- test/test_emails.py
+++ test/test_emails.py
@@ -1,13 +1,24 @@
 import re
+from model.contact import CONTACT
 
+def test_emails_on_home_page(app, db):
+    if len(db.get_addnew_list()) == 0:
+        app.addnew.create_new(CONTACT(name="Anna", middlename="Ivanovna", lastname="Petrova", nickname="AnPet",
+                           title="ZAO", company="Romashka", address="Moscow",
+                           home="84951234567", mobile="89161234567", work="88001234567", fax="88000000000",
+                           email="an@an.ru", email2="anp@an.ru", email3="aniv@an.ru",
+                           address2="Lenina street", phone2="83", notes="16"))
 
-def test_emails_on_home_page(app):
-    addnew_from_home_page = app.addnew.get_addnew_list()[0]
-    addnew_from_edit_page = app.addnew.get_addnew_info_from_edit_page(0)
-    assert addnew_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(addnew_from_edit_page)
-    assert addnew_from_home_page.name == addnew_from_edit_page.name
-    assert addnew_from_home_page.lastname == addnew_from_edit_page.lastname
-    assert addnew_from_home_page.address == addnew_from_edit_page.address
+        addnews_ui = sorted(app.addnew.get_addnew_list(), key=CONTACT.id_or_max)
+        addnews_db = sorted(db.get_addnew_list(), key=CONTACT.id_or_max)
+        assert len(addnews_ui) == len(addnews_db)
+        for i in range(len(addnews_ui)):
+            assert addnews_ui[i].name == addnews_db[i].name
+            assert addnews_ui[i].lastname == addnews_db[i].lastname
+            assert addnews_ui[i].address == addnews_db[i].address
+            assert clear(addnews_ui[i].all_emails_from_home_page) == merge_emails_like_on_home_page(addnews_db[i])
+            assert clear(addnews_ui[i].all_phones_from_home_page) == merge_phones_like_on_home_page(addnews_db[i])
+
 
 
 
@@ -19,3 +30,12 @@ def merge_emails_like_on_home_page(mail):
                             map(lambda x: clear(x),
                                 filter(lambda x: x is not None,
                                        [mail.email, mail.email2, mail.email3]))))
+
+
+def merge_phones_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear(x),
+                                filter(lambda x: x is not None,
+                                       [contact.home, contact.mobile, contact.work, contact.phone2]))))
+
+
